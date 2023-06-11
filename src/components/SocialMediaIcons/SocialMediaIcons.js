3eabc4b0e501b2774f330eaa673e7ded40efceba
Omkar Sonawane <45181659+Omkarcode11@users.noreply.github.com>
2023-06-11 22:36:46 +0530

    Update SocialMediaIcons.js
    change LinkedIn URL

--- src/components/SocialMediaIcons/SocialMediaIcons.js
+++ src/components/SocialMediaIcons/SocialMediaIcons.js
@@ -6,7 +6,7 @@ function SocialMediaIcons() {
     <div className="flex justify-center md:justify-start my-10 gap-7">
       <a
         className="hover:opacity-50 transition duration-500"
-        href="www.linkedin.com/in/omkar-sonawane-b75b05239"
+        href="https://www.linkedin.com/in/omkardev/"
         target="_blank"
         rel="noreferrer"
       >
